35c68dc885fcf5ae514e2c064fc6307d47a0cd9d
IEVN1002-22001719 <elemonsenava@gmail.com>
2026-09-18 20:50:30 -0600

    ng model 18/09/2026

--- src/app/heroes/heroes-list/heroes-list.ts
+++ src/app/heroes/heroes-list/heroes-list.ts
@@ -6,4 +6,48 @@ import { Component } from '@angular/core';
   styleUrl: './heroes-list.css',
   templateUrl: './heroes-list.html',
 })
-export class HeroesList {}
+export class HeroesList {
+ imageWidth:number=40;
+ imageMargin:number=2;
+ muestraImage:boolean=true;
+ listFilter:string="";
+
+ showImage():void{
+  this.muestraImage=!this.muestraImage
+ }
+
+  
+  
+  
+  heroes:any[]=[
+    {
+      imagen:"https://dragonball-api.com/characters/goku_normal.webp",
+      nombre:"Goku",
+      descripcion:"El protagonista de la serie, conocido por su gran poder y personalidad amigable. Originalmente enviado a la Tierra como un infante volador con la misión de conquistarla. Sin embargo, el caer por un barranco le proporcionó un brutal golpe que si bien casi lo mata, este alteró su memoria y anuló todos los instintos violentos de su especie, lo que lo hizo crecer con un corazón puro y bondadoso, pero conservando todos los poderes de su raza. No obstante, en la nueva continuidad de Dragon Ball se establece que él fue enviado por sus padres a la Tierra con el objetivo de sobrevivir a toda costa a la destrucción de su planeta por parte de Freeza. Más tarde, Kakarot, ahora conocido como Son Goku, se convertiría en el príncipe consorte del monte Fry-pan y líder de los Guerreros Z, así como el mayor defensor de la Tierra y del Universo 7, logrando mantenerlos a salvo de la destrucción en innumerables ocasiones, a pesar de no considerarse a sí mismo como un héroe o salvador.",
+      race:"Saiyan",
+      ki:6000000
+    },
+    {
+      imagen:"https://dragonball-api.com/characters/vegeta_normal.webp",
+      nombre:"Vegeta",
+      descripcion:"Príncipe de los Saiyans, inicialmente un villano, pero luego se une a los Z Fighters. A pesar de que a inicios de Dragon Ball Z, Vegeta cumple un papel antagónico, poco después decide rebelarse ante el Imperio de Freeza, volviéndose un aliado clave para los Guerreros Z. Con el paso del tiempo llegaría a cambiar su manera de ser, optando por permanecer y vivir en la Tierra para luchar a su lado contra las inminentes adversidades que superar. Junto con Piccolo, él es de los antiguos enemigos de Goku que ha evolucionando al pasar de ser un villano y antihéroe, a finalmente un héroe a lo largo del transcurso de la historia, convirtiéndose así en el deuteragonista de la serie.",
+      race:"Androide",
+      ki:280000000
+    },
+    {
+      imagen:"https://dragonball-api.com/characters/bulma.webp",
+      nombre:"Bulma",
+      descripcion:"Bulma es la protagonista femenina de la serie manga Dragon Ball y sus adaptaciones al anime Dragon Ball, Dragon Ball Z, Dragon Ball Super y Dragon Ball GT. Es hija del Dr. Brief y su esposa Panchy, hermana menor de Tights y una gran amiga de Son Goku con quien inicia la búsqueda de las Esferas del Dragón. En Dragon Ball Z tuvo a Trunks, primogénito de quien sería su esposo Vegeta, a su hija Bra[3] y su hijo adulto del tiempo alterno Trunks del Futuro Alternativo.",
+      race:"Humano",
+      ki:0
+    },
+    {
+      imagen:"https://dragonball-api.com/characters/Androide_18_Artwork.webp",
+      nombre:"Numero 18",
+      descripcion:"creada a partir de una base humana por el Dr. Gero con el único fin de destruir a Goku. y Ama a chrilin",
+      race:"Androide",
+      ki:280000000
+    },
+  ]
+
+}
